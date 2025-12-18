c8fe515680ede3fd400275f1b3bae42695657157
root <root@DESKTOP-T6Q9OUT.localdomain>
2025-12-18 18:55:38 +0300

    Добавлены текстуры на блоках

--- Block.cpp
+++ Block.cpp
@@ -1,18 +1,37 @@
 #include "Block.h"
-#include "Graph_lib/Graph.h"
+#include <FL/fl_draw.H>
+#include <FL/Fl_PNG_Image.H>
 
-Block::Block (Point xy, int width, int height, int hp) : Rectangle(xy, width, height), health(hp) {
-
+Block::Block(Point xy, int width, int height, int hp) : 
+    Rectangle(xy, width, height), health(hp) {
+    
+    int textureIndex = (xy.x / width + xy.y / height) % 4;
+    
+    const char* textures[] = {
+        "sources/textures/roflan_block.png",
+        "sources/textures/nerd_block.png",
+        "sources/textures/moustache_block.png",
+        "sources/textures/purple_block.png"
+    };
+    
+    textureName = textures[textureIndex];
 }
 
-int Block::get_health() const{
-    return health;
-}
-
-void Block::set_health(int new_health) {
-    health = new_health;
-}
-
-void Block::deal_damage() {
-    health = std::max(health - 1, 0);
+void Block::draw_lines() const {
+    int x = point(0).x;
+    int y = point(0).y;
+    int w = width();
+    int h = height();
+    
+    Fl_PNG_Image texture(textureName);
+    
+    if (texture.w() > 0) {
+        texture.draw(x, y, w, h);
+    } else {
+        fl_color(FL_RED);
+        fl_rectf(x, y, w, h);
+    }
+    
+    fl_color(FL_BLACK);
+    fl_rect(x, y, w, h);
 }
--- Block.h
+++ Block.h
@@ -2,19 +2,22 @@
 #define ARKANOID_BLOCK_H
 
 #include "Graph_lib/Graph.h"
+#include <string>
 
 using namespace Graph_lib;
 
-class Block : public Rectangle  {
+class Block : public Rectangle {
 public:
     Block(Point xy, int width, int height, int hp);
-
+    
+    void draw_lines() const override;
     int get_health() const;
     void set_health(int new_health);
     void deal_damage();
-
+    
 private:
     int health;
+    const char* textureName; // Храним имя текстуры для этого блока
 };
 
 #endif // ARKANOID_BLOCK_H
