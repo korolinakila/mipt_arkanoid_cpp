48bb5a996af660f145308116564a2f08f3e18315
root <root@DESKTOP-T6Q9OUT.localdomain>
2025-12-19 13:09:52 +0300

    добавлен скин кнопки

--- StartScreen.cpp
+++ StartScreen.cpp
@@ -7,18 +7,30 @@
 
 StartScreen::StartScreen(int x, int y, int w, int h, Graph_lib::Window& window) 
     : Fl_Group(x, y, w, h), background("sources/textures/StartScreen.png") {
+
+    Fl_PNG_Image* button_img = new Fl_PNG_Image("sources/textures/button_start.png");
     
-    Fl_Button *button = new Fl_Button(300, 350, 200, 100, "СТАРТ");
+    Fl_Button *button = new Fl_Button(300, 350, 200, 100);
+    if (button_img && !button_img->fail()) {
+        button->image(button_img);
+
+        
+        button->label(nullptr);
+    } else {
+        button->label("СТАРТ");
+        button->labelfont(FL_BOLD);
+        button->labelsize(20);
+        button->labelcolor(FL_WHITE);
+    }
+    
     button->callback([](Fl_Widget *w, void *data) {
         static_cast<ArkanoidWindow*>(data)->openGameScreen();
     }, &window);
 }
 
 void StartScreen::draw() {
-
     if (!background.fail()) {
         background.draw(x(), y(), w(), h());
     }
-
     Fl_Group::draw();
 }
